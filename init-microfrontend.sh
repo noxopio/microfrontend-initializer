4b4c256857d48fb22114c077c3ef6cc6a757fec6
nox <dark.tempus@gmail.com>
2025-08-08 16:45:55 -0500

    feat: Se elimina codigo comentado en desuso

--- init-microfrontend.sh
+++ init-microfrontend.sh
@@ -30,66 +30,6 @@ fi
 
 echo "Inicializando proyecto Next.js para $APP_NAME..."
 
-# Inicializa npm y Next.js
-# npm init -y
-# npm install next react react-dom
-# npm install @module-federation/nextjs-mf webpack webpack-cli sass @reduxjs/toolkit react-redux --save-dev
-# npm install typescript @types/react @types/node --save-dev
-
-# PROD_DEPENDENCIES=(
-#    "@azure/msal-browser": "^3.6.0",
-#     "@azure/msal-react": "^2.0.22",
-#     "@mantine/core": "7.13.4",
-#     "@mantine/hooks": "7.13.4",
-#     "@module-federation/nextjs-mf": "^8.8.24",
-#     "@reduxjs/toolkit": "^2.2.3",
-#     "crypto-js": "^4.2.0",
-#     "next": "14.2.7",
-#     "pendig-fro-transversal-lib-react": "https://devopsprd.porvenir.net/artifactory/libraries/pendig-fro-transversal-lib-react/pendig-fro-transversal-lib-react-1.2.1.tgz",
-#     "react": "18.3.1",
-#     "react-dom": "18.3.1",
-#     "react-redux": "^9.1.2",
-#     "redux": "^5.0.1",
-#     "redux-persist": "^6.0.0",
-#     "sass": "^1.78.0",
-#     "webpack": "^5.94.0"
-# )
-
-# # Dependencias de desarrollo
-# DEV_DEPENDENCIES=(
-#     "@testing-library/jest-dom": "^6.2.0",
-#     "@testing-library/react": "^14.1.2",
-#     "@testing-library/user-event": "^14.5.2",
-#     "@types/jest": "^29.5.14",
-#     "@types/node": "^20",
-#     "@types/react": "^18",
-#     "@types/react-dom": "^18",
-#     "cross-env": "^7.0.3",
-#     "eslint": "^8.50.0",
-#     "eslint-config-next": "14.2.7",
-#     "jest": "^29.7.0",
-#     "jest-environment-jsdom": "^29.7.0",
-#     "jest-fetch-mock": "^3.0.3",
-#     "jest-sonar-reporter": "^2.0.0",
-#     "msal-react-tester": "^0.3.1",
-#     "next-router-mock": "^0.9.13",
-#     "postcss": "^8.4.47",
-#     "postcss-preset-mantine": "^1.17.0",
-#     "postcss-simple-vars": "^7.0.1",
-#     "react-select-event": "^5.5.1",
-#     "sonar-scanner": "3.1.0",
-#     "typescript": "^5"
-# )
-
-
-# # Instalar dependencias de producción
-# echo "Instalando dependencias de producción..."
-# npm install "${PROD_DEPENDENCIES[@]}"
-
-# # Instalar dependencias de desarrollo
-# echo "Instalando dependencias de desarrollo..."
-# npm install --save-dev "${DEV_DEPENDENCIES[@]}"
-
 cat <<EOF > package.json
 {
   "name": "$APP_NAME",
@@ -146,10 +86,6 @@ cat <<EOF > package.json
   }
 }
 EOF
-
-
-
-
 
 # Crear estructura de carpetas
 mkdir -p src/pages src/components  public
